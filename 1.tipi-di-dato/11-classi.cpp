4614b72870b0d3296bbe7ff1be0d3dc54f7b6bf5
Luca Suriano <l.s@cryptolab.net>
2010-10-19 18:56:33 +0200

    Definizione del costrutto di classe

---
+++ 1.tipi-di-dato/11-classi.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+/* IL COSTRUTTO DI CLASSE
+ * Saranno ora presenate le nozioni essenziali per la realizzazione di una
+ * classe in C++, tutto cio' che serve per poter continuare lo studio del
+ * linguaggio, senza pero' tuttavia entrare eccessivamente nel dettaglio, ma
+ * quanto basta per poter proseguire la lettura delle sorgenti.
+ *
+ * La classe si definisce mediante la parola chiave class piu' un
+ * identificatore, che assume il ruolo di specificatore di tipo; il corpo e'
+ * caratterizzato da una sezione publica ed una sezione private.
+ *
+ * class Identificatore
+ * {
+ * public:
+ *
+ * 		// contiene i metodi della classe o funzioni membro.
+ *		// L'interfaccia pubblica.
+ *	
+ * private:
+ *
+ * 		// I dati, non accessibili dall'esterno della classe.
+ *
+ * };
+ *
+ * I metodi della sezione public sono le operazioni con cui un utente puo'
+ * interagire con l'oggetto della classe.
+ */
+
+// Si definisce una classe String, per gestire alcune operazioni sulle stringhe.
+
+
+int main() {
+
+   return(0);
+}
+
