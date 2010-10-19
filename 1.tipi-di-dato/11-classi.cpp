b24e6d6fa806f2db7305b5e29973e32a41c77616
Luca Suriano <l.s@cryptolab.net>
2010-10-19 19:50:02 +0200

    Conclusa la progettazione della classe String

--- 1.tipi-di-dato/11-classi.cpp
+++ 1.tipi-di-dato/11-classi.cpp
@@ -30,6 +30,50 @@ using namespace std;
 
 // Si definisce una classe String, per gestire alcune operazioni sulle stringhe.
 
+Class String;
+istream& operator>>(istream&, String&);
+ostream& operator<<(ostream&, const String&);
+
+class String
+{
+	public:
+		// Definizione dei costruttori
+		// Si definiscono 3 costruttori sfruttando l'overloading degli operatori -
+		// letteramente sovraccarico - , ogni istanza infatti e' diversa dall'altra
+		// in base alla lista dei parametri.
+		//
+		// String() e' il costruttore di default, non richiede un esplicita
+		// inizializzazione iniziale
+		//
+		// Si noti il terzo costruttore, String(const String&), e' detto
+		// costruttore di copia, poiche' inizializza un oggetto della classe
+		// come copia per un secondo oggetto.
+		String();
+		String(const char*);
+		String(const String&);
+
+		// Distruttore
+		~String();
+
+		// L'overloaded di alcuni operatori di assegnamento
+		Stringa& operator=(const String&);
+		Stringa& operator=(const char*);
+
+		// L'overloaded di alcuni operatori di uguaglianza
+		bool operator==(const String&);
+		bool operator==(const char*);
+
+		// L'overloaded dell'operatore di subscript[]
+		char& operator[](int);
+
+		// Metodi di accesso
+		int size() { return _size; };
+		char* c_str() { return _string };
+
+	private:
+		int _size;
+		char *_string;
+}
 
 int main() {
 
