9ed51096327f1f7b92552e92b06a69b8a9a32cc2
Luca Suriano <l.s@cryptolab.net>
2010-10-24 22:15:59 +0200

    Concetto generale di espressione

---
+++ 2.espressioni/01-espressione.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	/*
+	 * Un espressione, solitamente, e' caratterizata da uno o piu' operandi e
+	 * naturalmente da una operazione da applicare agli operandi stessi.
+	 *
+	 * Operatori che agiscono su un operando sono detti unari
+	 * Operatori che agiscono su due operandi sono detti binari
+	 *
+	 * L'operatore di indirizzo ad esempio e' un operatore unario, l'operatore
+	 * somma invece e' binario, da notare inoltre che alcuni di essi possono
+	 * essere sia unari sia binari, come l'operatore di derefernziazione '*',
+	 * che e' anche operatore di moltiplicazione.
+	 * 
+	 * La valutazione di una espressione porta ad un risultato, tipicamente un
+	 * rvalue.
+	 */
+
+
+
+   return(0);
+}
+
