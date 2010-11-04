5be79d4c0097e3238037e6ff1f132f2edb3c17ab
Luca Suriano <l.s@cryptolab.net>
2010-11-04 20:24:17 +0100

    Inserita la tabella sulla precedenza degli operatori

--- 2-Operatori-predefiniti/01-espressioni-operatori.cpp
+++ 2-Operatori-predefiniti/01-espressioni-operatori.cpp
@@ -51,22 +51,32 @@ int main() {
 
         int var = 5, var1 = 10, var2 = 22;
         
-        // OPERATORE SOMMA
+		///////////////////////////////////////////////////////////////////////
+        // OPERATORE SOMMA +
+		///////////////////////////////////////////////////////////////////////
 		int var3 = var1 + var2;
         
-        // OPERATORE SOTTRAZIONE
+		///////////////////////////////////////////////////////////////////////
+        // OPERATORE SOTTRAZIONE -
+		///////////////////////////////////////////////////////////////////////
         int var4 = var3 - var2;
         
-        // OPERATORE MOLTIPLICAZIONE
+		///////////////////////////////////////////////////////////////////////
+        // OPERATORE MOLTIPLICAZIONE *
+		///////////////////////////////////////////////////////////////////////
 		int var5 = var1 * var2;
         
-        // OPERATORE DIVISIONE
-        // Il risultato della divisione tra due interi dara' un intero, qualora il
-        // quoziente contenesse una parte frazionaria, sara' troncata.
+		///////////////////////////////////////////////////////////////////////
+        // OPERATORE DIVISIONE /
+		///////////////////////////////////////////////////////////////////////
+        // Il risultato della divisione tra due interi dara' un intero, qualora 
+		// il quoziente contenesse una parte frazionaria, sara' troncata.
         
 		int var6 = var2 / var1;
         
-        // OPERATORE RESTO (o modulo)
+		///////////////////////////////////////////////////////////////////////
+        // OPERATORE RESTO (o modulo) %
+		///////////////////////////////////////////////////////////////////////
         // Calcola il resto della divisione tra interi, applicabile solo a:
         // short; int; long; char.
         
@@ -74,22 +84,21 @@ int main() {
         int var8 = 21 % -5;             // 1 o -1 dipendente dalla macchina
         
         // Eccezioni aritmetiche
-        // Un char puo' contenere 8 bit, per cui un valore che e' compreso tra 127
-        // e 255, byte supera questo valore andando in overflow
+        // Un char puo' contenere 8 bit, per cui un valore che e' compreso tra 
+		// 127 e 255, byte supera questo valore andando in overflow
         
 		char byte = 32;
         int val = 8;
         byte = val * byte; // Overflow 
         
-		/* OPERATORE DI ASSEGNAMENTO
-		 * =
-		 *
-		 * L'operatore di assegnamento sovrascrive il valore di un oggetto con
-		 * un nuovo valore, da non confondersi con l'inizializzazione, che puo'
-		 * essere effetuata una sola volta durante la definizione, 
-		 * l'assegnamento di un valore invece puo' essere eseguito diverse volte
-		 * durante l'esecuzione di un programma.
-		 */
+		///////////////////////////////////////////////////////////////////////
+		// OPERATORE DI ASSEGNAMENTO =
+		///////////////////////////////////////////////////////////////////////
+		// L'operatore di assegnamento sovrascrive il valore di un oggetto con
+		// un nuovo valore, da non confondersi con l'inizializzazione, che puo'
+		// essere effetuata una sola volta durante la definizione, 
+		// l'assegnamento di un valore invece puo' essere eseguito diverse 
+		// volte durante l'esecuzione di un programma.
 		
 		int int_var = 9, dbl_var = 9.1982738, mix_var = 0;
 		
@@ -119,13 +128,17 @@ int main() {
 		var_b--;
 		cout << "var_a: " << var_a << " var_b: " << var_b << " var_c: "<< var_c << endl;
 
+		///////////////////////////////////////////////////////////////////////
 		// OPERATORE CONDIZIONALE
+		///////////////////////////////////////////////////////////////////////
 		// expr ? expr2 : expr3
 		// Se expr e' vera ritorna expr2, se falsa expr3
 		cout << ((var_a > var_b) ? true : false) << endl;
 		cout << ((var_b > var_a) ? true : false) << endl;
 
-		// OPERATORE DI SERIALIZZAZIONE , (o operatore virgola)
+		///////////////////////////////////////////////////////////////////////
+		// OPERATORE DI SERIALIZZAZIONE (o operatore virgola) ,
+		///////////////////////////////////////////////////////////////////////
 		// Il lavoro dell'operatore virgola è di far sì che diverse espressioni,
 		// siano valutate da sinistra a destra, il risultato ed anche il tipo
 		// corrisponde con l'espressione più a destra della lista; un caso
@@ -144,6 +157,31 @@ int main() {
 		double var_opser_b = (-89.10, var_opser_a, 10.9);
 		cout << "Variabile intera: " << var_opser_a << "\nVariabile double: " 
 			<< var_opser_b << endl;
+
+		///////////////////////////////////////////////////////////////////////
+		// TABELLA SULLA PRECEDENZA DEGLI OPERATORI
+		///////////////////////////////////////////////////////////////////////
+		cout << "\nGli operatori in alto hanno la precedenza verso quelli in basso\n" << endl;
+		cout << "  OPERATORE      " << "    FUNZIONE          " << "     USO" << endl;
+		cout << "-----------------" << "----------------------" << "-----------------------------" << endl;
+		cout << "::               " << " scopo globale        " << " ::nome" << endl;
+		cout << "::               " << " scopo di classe      " << " classe::nome" << endl;
+		cout << "::               " << " scopo di namespace   " << " namespace::nome" << endl;
+		cout << "-----------------" << "----------------------" << "-----------------------------" << endl;
+		cout << ".                " << " seletori di membro   " << " oggetto.membro" << endl;
+		cout << "->               " << " seletori di membro   " << " puntatore->membro" << endl;
+		cout << "[]               " << " subscript            " << " variabile[expr]" << endl;
+		cout << "()               " << " chiamata di funzione " << " nome(lista_expr)" << endl;
+		cout << "()               " << " costruzione di tipo  " << " tipo(lista_expr)" << endl;
+		cout << "++               " << " incremento postfisso " << " lvalue++" << endl;
+		cout << "--               " << " decremento postfisso " << " lvalue--" << endl;
+		cout << "typeid           " << " type ID              " << " typeid(tipo)" << endl;
+		cout << "typeid           " << " type ID run time     " << " typeid(tipo)" << endl;
+		cout << "const_cast       " << " conversione di tipo  " << " const_cast<tipo>(expr)" << endl;
+		cout << "dynamic_cast     " << " conversione di tipo  " << " dynamic_cast<tipo>(expr)" << endl;
+		cout << "reinterpret_cast " << " conversione di tipo  " << " reinterpret_cast<tipo>(expr)" << endl;
+		cout << "static_cast      " << " conversione di tipo  " << " static_cast<tipo>(expr)" << endl;
+
 
 		return(0);
 }
