8270ba4e4d704f9864debb6f023d1b488b135f99
Luca Suriano <l.s@cryptolab.net>
2010-11-11 18:16:19 +0100

    Inserite tutte le operazioni per la gestione dello stack

--- 2-Operatori-predefiniti/stack.cpp
+++ 2-Operatori-predefiniti/stack.cpp
@@ -9,11 +9,11 @@ using namespace std;
 // mondo reale si puo' immaginare la classica pila di piatti.
 
 // La classe intStack sara' in grado di gestire elementi di tipo intero
-class iStack 
+class Stackint 
 {
 	public:
 		// Costruttore
-		iStack(int capacity)
+		Stackint(int capacity)
 			: _stack(capacity), _top(0) {};
 
 		// Preleva un elemento
@@ -39,28 +39,79 @@ class iStack
 
 // _top contiene il numero degli elementi sulla pila, pertanto size() restiuisce
 // semplicemente _top
-inline int iStack::size() 
+inline int Stackint::size() 
 { 
 	return _top; 
 };
 
 // empty() restituisce true se _top e' uguale a 0, false alrimenti
-inline bool iStack::empty() 
+inline bool Stackint::empty() 
 {
 	return (_top ? false: true);
 };
 
-// full() restituisce true se _top e' uguale a stack.size(), ovvero se utti gli
+// full() restituisce true se _top e' uguale a stack.size(), ovvero se tutti gli
 // elementi sono stati inseriti nello stack
-inline bool iStack::full() 
+inline bool Stackint::full() 
 {
 	return ((_top < _stack.size()-1) ? false : true);
 };
 
+// Recupera il primo elemento nello stack
+bool Stackint::pop(int &value)
+{
+	if ( empty() )
+		return false;
 
+	value = _stack[--_top];
+	cout << "pop(): " << value << endl;
+
+	return true;
+}
+
+// Inserisce un elemento in cima allo stack
+bool Stackint::push(int value)
+{
+	cout << "push(): " << value << endl;
+
+	if ( full() )
+		return false;
+
+	_stack[_top++] = value;
+
+	return true;
+}
+
+// Visualizza ciascun elemento della pila, dalla base alla cima
+void Stackint::display()
+{
+	if ( !size() )  
+		cout << "(0)\n";
+
+	cout << "[totale elementi: " << size()+1 << "] (BASE: ";
+	
+	for (int x = 0; x<_top; ++x)
+		cout << _stack[x] << " " ;
+	cout << " :TOP)\n";
+}
 
 int main() {
-	
+	Stackint stack(20);
+	stack.push(1);
+
+	int x;
+
+	for (x=0; x<10; x++) {
+		if ( stack.empty() == false)
+			stack.push(x);
+		stack.display();
+	}
+
+	for (x=10; x>0; x--) {
+		if ( stack.empty() == false)
+			stack.pop(x);
+		stack.display();
+	}
 	
 	return(0);
 }
