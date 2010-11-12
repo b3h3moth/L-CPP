12bca5bcadcc61174d517ea52de79e3dc4857fcf
Luca Suriano <l.s@cryptolab.net>
2010-11-12 19:26:04 +0100

    Istruzioni semplici e istruzioni composte

---
+++ 3-Istruzioni/01-istruzioni-semplice-composte.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// L'istruzione piu' semplice che puo' essere realizzata e' una istruzione
+	// vuota, che corrisponde al semplice punto e virgola.
+	
+	;
+
+	// L'istruzione e' l'unità piu' piccola di un programma C++, ciascuna
+	// istruzione termina con il punto e virgola finale; da notare tuttavia che,
+	// per cio' che concerne le istruzioni composte, non e' necessario inserire
+	// il punto e virgola dopo la graffa di chiusura.
+
+	// ISTRUZIONE SEMPLICE
+	// Consiste in una sola istruzione
+	
+	int val_int = 10;	// Istruzione di dichiarazione
+	val_int;			// Istruzione espressione
+	val_int += 10;		// Istruzione di assegnamento
+
+	// ISTRUZIONE COMPOSTA
+	// E' caratterizzata da una sequenza di istruzioni semplici racchiuse tra
+	// parentesi graffe, nel caso specifico e' formata da una dichiarazione e 
+	// da un assegnamento; una istruzione composta che contiene piu' di una
+	// istruzione di dichiarazione e' detta anche blocco di istruzioni o piu'
+	// semplicemente blocco. 
+	
+	if (val_int > 0)
+	{
+		int val_int_b = val_int;
+		val_int = 20;
+	}
+
+	// Un blocco di istruzioni gode di una proprio campo d'azione, per cui le
+	// variabili dichiarate al suo interno sono visibili soltanto all'interno
+	// del blocco stesso.
+
+	// Le istruzioni composte possono essere inserite in qualsiasi punto del 
+	// programma, cosi' come le istruzioni semplici; anche con le istruzioni
+	// composte e' ammissibile realizzare una istruzione nulla
+	
+	{}
+
+	// A che scopo utilizzare una istruzione nulla, sia essa semplice o
+	// composta? E' utile soprattutto allorquando la sintassi del programma la
+	// richiede anche se logicamente se ne potrebbe fare a meno.
+	
+	return(0);
+}
+
+// TO DO: Produrre un esempio esplicativo sul concetto di istruzione nulla.
