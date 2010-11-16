a2f3ed1d2613a8ca1dcec018b2e07d884c6b3b5c
Luca Suriano <l.s@cryptolab.net>
2010-11-16 18:46:15 +0100

    Introduzione all'istruzione if

---
+++ 3-Istruzioni/02-if.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	// L'istruzione if, in base alla verita' di una espressione logica, consente
+	// l'esecuzione condizionale di una istruzione o di un blocco di istruzioni;
+	// la sintassi e' la seguente:
+	//
+	// if ( condizione )
+	//      istruzione;
+	//
+	// - La condizione deve essere sempre racchiusa tra parentesi tonde;
+	// - La condizione puo' essere una espressione if (x + y > N);
+	// - La condizione puo' essere una dichiarazione  if ( val = get_value() );
+	// - L'oggetto o gli oggetti definiti all'interno della condizione hanno il
+	// loro campo di azione e pertanto sono visibili solo all'interno del blocco
+	// associato;
+	
+	
+	return(0);
+}
+
