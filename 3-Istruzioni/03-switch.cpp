56e6993fd40d5dc462cc1d633a215324faad761a
Luca Suriano <l.s@cryptolab.net>
2010-11-25 23:43:37 +0100

    Istruzioni: completato un esempio per verificare switch all'opera

--- 3-Istruzioni/03-switch.cpp
+++ 3-Istruzioni/03-switch.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <ctype.h>
+
 using namespace std;
 
 int main() {
@@ -35,7 +37,8 @@ int main() {
 	// - Si ponga particolare attenzione alla terminazione di ciascun case,
 	// utilizzando un break o un return, per evitare l'esecuzione a cascata di
 	// tutte le etichette; e' compito del programmatore far terminare lo
-	// switch.
+	// switch, e se proprio si necessita di una etichetta senza break e' sempre
+	// meglio aggiungere un commento per spiegare nel dettaglio la motivazione.
 	// 
 	// Il risultato della espressione switch sara' confrontato con ogni valore
 	// associato alle etichette, qualora si trovi corrispondenza tra
@@ -45,7 +48,7 @@ int main() {
 	// se inserita.
 	//
 	// Da notare che un'istruzione switch, se ben congeniata, potra' sempre
-	// essere trasformata in un gruppo di istruzione if; con essere ben
+	// essere trasformata in un gruppo di istruzioni if; con essere ben
 	// congeniata s'intende sia sintatticamente sia logicamente corretta:
 	//
 	// switch (expr) {
@@ -66,8 +69,58 @@ int main() {
 	// Pertanto, l'istruzione break fa terminare l'esecuzione della switch ed il
 	// controllo passa immediatamente alla prima istruzione subito dopo la
 	// parentesi graffa finale.
-
+	//
+	// Come esempio si realizza un programma che conta le vocali all'interno di
+	// un testo e le consonanti:
 	
+	char ch;
+	int ch_a = 0, ch_e = 0, ch_i = 0, ch_o = 0, ch_u = 0;
+	int ch_consonante = 0;
+
+	while (cin >> ch)
+		switch(ch) 
+		{
+			case 'a': case 'A':
+				++ch_a;
+				break;
+			case 'e': case 'E':
+				++ch_e;
+				break;
+			case 'i': case 'I':
+				++ch_i;
+				break;
+			case 'o': case 'O':
+				++ch_o;
+				break;
+			case 'u': case 'U':
+				++ch_u;
+				break;
+			default:
+				if (isalpha(ch))
+					++ch_consonante;
+				break;
+		}
+
+	cout << "    Tot vocale \"a\": " << ch_a << endl;
+	cout << "    Tot vocale \"e\": " << ch_e << endl;
+	cout << "    Tot vocale \"i\": " << ch_i << endl;
+	cout << "    Tot vocale \"o\": " << ch_o << endl;
+	cout << "    Tot vocale \"u\": " << ch_u << endl;
+	cout << "Tot consonanti \"a\": " << ch_consonante << endl;
+
+	// Compilato ed eseguito con questo listato:
+	//
+	// $ g++ 03-switch.cpp -o switch
+	// ./switch < 03-switch.cpp
+	//
+	//     Tot vocale "a": 220
+	//     Tot vocale "e": 300
+	//     Tot vocale "i": 193
+	//     Tot vocale "o": 168
+	//     Tot vocale "u": 78
+	//     Tot consonanti "a": 1224
+	
+
 	return(0);
 }
 
