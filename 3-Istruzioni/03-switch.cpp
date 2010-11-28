d57c7fd47f164560e9ba2e6e0a72c576fbe2d4ab
Luca Suriano <l.s@cryptolab.net>
2010-11-28 15:36:42 +0100

    Istruzioni: corretti taluni refusi ed esemplificati alcuni periodi

--- 3-Istruzioni/03-switch.cpp
+++ 3-Istruzioni/03-switch.cpp
@@ -6,50 +6,52 @@ using namespace std;
 int main() {
 	
 	// SWITCH
-	// Consente un metodo alternativo alle clausole if-else per la scelta di una
-	// opzione tra una serie di opzioni.
+	// L'istruzione switch consente un metodo alternativo alle clausole if-else,
+	// essa consente di scegliere una tra diverse opzioni definite in fase
+	// inmplementativa.
 	//
 	// Sintassi:
 	//
 	// switch (expr)
 	// 	statement
 	//
-	// Le caratteristiche di implementazione:
+	// Le caratteristiche implementative e logiche possono essere sintetizzate
+	// mediante le seguenti proprieta':
 	//
 	// - La parola chiave switch e' seguita tra parentesi da un'espressione che
 	// dovra' essere valutata;
 	//
 	// - Valutata l'espressione vi e' un insieme di case (etichette), ciascuna
-	// delle quali seguita da una espressione costante di tipo intero, che
-	// posta a confronto con il risultato dell'espressione dello switch;
+	// delle quali seguita da una espressione costante di tipo intero, posta a
+	// confronto con il risultato dell'espressione dello switch;
 	//
-	// - Ciascun case si chiude con i due punti ":".
+	// - Ogni istruzione case si chiude con i due punti ":".
 	//
 	// - Le etichette possono essere associate;
 	//
 	// - L'etichetta finale, opzionale ma raccomandata, e' definita come
-	// etichetta di default, ovvero se nessuna delle etichette coincide con 
+	// etichetta di default, se nessuna delle etichette coincidera' con 
 	// l'espressione dello switch, essa sara' eseguita;
 	//
-	// - Sara' errore a tempo di compilazione qualora due etichette abbiano lo
-	// stesso valore.
+	// - Sara' errore a tempo di compilazione qualora due etichette dovessero
+	// avere lo stesso valore.
 	//
 	// - Si ponga particolare attenzione alla terminazione di ciascun case,
 	// utilizzando un break o un return, per evitare l'esecuzione a cascata di
-	// tutte le etichette; e' compito del programmatore far terminare lo
+	// tutte le etichette. E' compito del programmatore far terminare lo
 	// switch, e se proprio si necessita di una etichetta senza break e' sempre
 	// meglio aggiungere un commento per spiegare nel dettaglio la motivazione.
 	// 
-	// Il risultato della espressione switch sara' confrontato con ogni valore
-	// associato alle etichette, qualora si trovi corrispondenza tra
+	// Il risultato della espressione dello switch sara' confrontato con ogni
+	// valore associato alle etichette e, qualora si trovi corrispondenza tra
 	// l'espressione stessa e un valore del case, sara' eseguita la prima
-	// istruzione subito dopo il valore del case; se invece non risulta
-	// corrispondenza si esegue la prima istruzione dell'etichetta di default,
-	// se inserita.
+	// istruzione subito dopo il valore del case, se invece non risultasse
+	// corrispondenza sara' eseguita la prima istruzione dell'etichetta di 
+	// default, se inserita.
 	//
 	// Da notare che un'istruzione switch, se ben congeniata, potra' sempre
-	// essere trasformata in un gruppo di istruzioni if; con essere ben
-	// congeniata s'intende sia sintatticamente sia logicamente corretta:
+	// essere trasformata in un gruppo di istruzioni if; con "essere ben
+	// congeniata" s'intende sia sintatticamente sia logicamente corretta:
 	//
 	// switch (expr) {
 	// 	case '1':
@@ -66,12 +68,12 @@ int main() {
 	// 		break;
 	// }
 	//
-	// Pertanto, l'istruzione break fa terminare l'esecuzione della switch ed il
-	// controllo passa immediatamente alla prima istruzione subito dopo la
-	// parentesi graffa finale.
+	// L'istruzione break consente la terminazione dell'esecuzione della switch,
+	// dopodichè il controllo passa immediatamente alla prima istruzione subito
+	// dopo la parentesi graffa finale.
 	//
 	// Come esempio si realizza un programma che conta le vocali all'interno di
-	// un testo e le consonanti:
+	// un testo passato in input:
 	
 	char ch;
 	int ch_a = 0, ch_e = 0, ch_i = 0, ch_o = 0, ch_u = 0;
@@ -113,14 +115,12 @@ int main() {
 	// $ g++ 03-switch.cpp -o switch
 	// ./switch < 03-switch.cpp
 	//
-	//     Tot vocale "a": 220
-	//     Tot vocale "e": 300
-	//     Tot vocale "i": 193
-	//     Tot vocale "o": 168
-	//     Tot vocale "u": 78
-	//     Tot consonanti "a": 1224
-	
+	//     Tot vocale "a": 234
+	//     Tot vocale "e": 332
+	//     Tot vocale "i": 211
+	//     Tot vocale "o": 190
+	//     Tot vocale "u": 81
+	// Tot consonanti "a": 1335
 
 	return(0);
 }
-
