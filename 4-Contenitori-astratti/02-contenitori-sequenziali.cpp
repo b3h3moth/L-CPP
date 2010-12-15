8dc19c4229d825ba4c5daf8f819a1761b3f95b85
Luca Suriano <l.s@cryptolab.net>
2010-12-15 11:50:56 +0100

    Contenitori astratti: definizione di contenitori sequenziali

---
+++ 4-Contenitori-astratti/02-contenitori-sequenziali.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+#include <vector>
+#include <list>
+#include <deque>
+#include <set>
+#include <map>
+
+int main() {
+	
+	// CONTENITORI SEQUENZIALI (SEQUENCES CONTAINER)
+	////////////////////////////////////////////////////////////////////////////
+	// Per lavorare con i contenitori sequenziali e' neccessario includere nel
+	// codice sorgente uno dei seguenti file header:
+	//
+	// #include <vector>
+	// #include <list>
+	// #include <deque>
+	// #include <set>
+	// #include <map>
+	//
+	// Un oggetto di tipo contenitore si definisce inserendo il nome del tipo
+	// del contenitore, seguito, all'interno di parentesi angolari, dal tipo di
+	// dato degli elementi del vettore, e infine dall'identificatore.
+	vector<string> str_vector;
+	list<int> int_list;
+	deque<double> int_deque;
+	set<int> int_set;
+	map<string,int> int_map;
+
+	// Il modo piu' semplice per inse
+
+	return(0);
+}
+
