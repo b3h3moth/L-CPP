0ef967195e5d4e81662fadce664a7b352257f414
Luca Suriano <l.s@cryptolab.net>
2011-09-11 17:37:37 +0200

    ostream_iterator, per lavorare sul canale di input e di output

--- 4-Contenitori-astratti/03-iteratori.cpp
+++ 4-Contenitori-astratti/03-iteratori.cpp
@@ -1,13 +1,17 @@
 #include <iostream>
-using namespace std;
 #include <vector>
+#include <string>
+#include <iterator>
+using namespace std;
 
 int main() {
 
 	// ITERATORI
 	////////////////////////////////////////////////////////////////////////////
-	// Forniscono un metodo per accedere in successione, ossia uno dopo l'altro,
-	// ad ogni elemento di un contenitore associativo o sequenziale.
+	// Forniscono un metodo per accedere in successione, ad ogni elemento di un
+	// contenitore associativo o sequenziale; in pratica sono un concetto
+	// estremamente utile per gestire sequenze di elementi all'interno dei
+	// contenitori.
 	//
 	// Nota: Ciascun contenitore fornisce un metodo begin() ed un metodo end(),
 	// accesso al primo e all'ultimo elemento di un contenitore rispettivamente.
@@ -15,11 +19,11 @@ int main() {
 	cout << "iteratore" << endl;
 	vector<int> int_vector;
 
-	// Si riempie il vetore con 10 interi
+	// Si riempie il vetore con 10 interi:
 	for( int i=0; i<10; i++)
 		int_vector.push_back(i);
 
-	// iterator e' un tipo definito all'interno della classe vector
+	// iterator e' un tipo definito all'interno della classe vector.
 	vector<int>::iterator iter = int_vector.begin();
 	vector<int>::iterator iter_end = int_vector.end();
 
@@ -39,10 +43,10 @@ int main() {
 	cout << "const_iterator, accesso ad elementi costanti" << endl;
 	
 	// Si crea un vector costante, inizializzandolo con gli elementi del vettore
-	// precedentemente creato.
+	// precedentemente creato:
 	const vector<int> int_vector_2(int_vector);
 	
-	// Si utilizza, poiche' gli elementi sono costanti, const_iterator
+	// Si utilizza, poiche' gli elementi sono costanti, const_iterator:
 	vector<int>::const_iterator iterc = int_vector_2.begin();
 	vector<int>::const_iterator iterc_end = int_vector_2.end();
 
@@ -52,6 +56,18 @@ int main() {
 	// Da notare che non avrebbe compilato se fosse stato utilizzato il solo 
 	// iterator. L'aritmetica degli iteratori e' del tutto simile all'aritmetica
 	// dei puntatori.
+
+	// ostream_iterator; ITERATORI SUI CANALI DI INPUT O OUTPUT
+	// Le sequenze di elementi non sono solo nei contenitori, ad esempio uno
+	// stream fornisce una sequenza di valori, in ingresso ed in uscita.
+	// Si definisce un iteratore del canale di uscita (e' necessario aggiungere
+	// l'header <iterator>:
+	ostream_iterator<string> output_channel(cout);
+
+	cout << "A video:" << endl; 
+	*output_channel = "C++ ";
+	*output_channel = "Language ";
+	*output_channel = "Programming";
 
 	return(0);
 }
