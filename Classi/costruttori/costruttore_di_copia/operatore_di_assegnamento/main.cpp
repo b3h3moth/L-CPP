4e36271b127d112e4d407c5029bcea3fec253160
Luca Suriano <behemoth@autistici.org>
2012-10-11 11:58:46 +0200

    costruttore di copia, operatore di assegnamento

--- Classi/costruttori/costruttore_di_copia/operatore_di_assegnamento/main.cpp
+++ Classi/costruttori/costruttore_di_copia/operatore_di_assegnamento/main.cpp
@@ -3,14 +3,20 @@
 using namespace std;
 
 int main() {
+
     Linux test("BSD", "OpenBSD");
     Linux test2;
     Linux test3;
     
     test.print();
     test2.print();
-    
+
+    /* Il costruttore di copia e' un costruttore particolare che viene 
+    invocato quando un nuovo ogetto e' istanziato ed inizializzato con un altro
+    oggetto; viene utilizzato l'operatore '=' che assegna i dati di un oggetto
+    ad un altro oggetto. */
     test2 = test;
+
     test.print();
     test2.print();
 
