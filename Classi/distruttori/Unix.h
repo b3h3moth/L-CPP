748844fdee2d792fb8d744c9be4598f36ca934a7
Luca Suriano <behemoth@autistici.org>
2012-10-13 13:28:54 +0200

    distruttori: proprieta'

--- Classi/distruttori/Unix.h
+++ Classi/distruttori/Unix.h
@@ -7,12 +7,15 @@ class Unix
         // Costruttore
         Unix(char *k_name, char *k_rel);
 
-        /* Un distruttore e' una funzione membro che e' automaticamente 
-        invocata quando un oggetto e' distrutto/eliminato; ha lo stesso nome
-        della classe preceduto dal simbolo tilde '~', inoltre come un 
-        costruttore, non ha tipo di ritorno, tuttavia i cosruttori non accettano
-        argomenti.Sono necessari solo se l'oggetto presenta un'estensione 
-        dinamica.*/
+        /* Un distruttore e' una funzione membro che viene automaticamente 
+        invocata quando un oggetto e' eliminato; ha lo stesso nome della classe
+        preceduto dal simbolo tilde '~', inoltre, come un  costruttore, non ha 
+        tipo di ritorno, tuttavia i cosruttori non accettano argomenti.
+        
+        Sono necessari solo se l'oggetto presenta un'estensione dinamica; non 
+        puo' essere invocato esplicitamente dal programma utente, ma viene
+        invocato implicitamente dal compilatore quando si dealloca lo spazio di 
+        memoria assegnato ad un oggetto. */
         ~Unix();
 
         const char *getKernelName() const { return kernel_name; }
