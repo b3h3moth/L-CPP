3f50a5420ab8869d5351c22096230734e4571ece
Luca Suriano <behemoth@autistici.org>
2012-10-18 10:58:32 +0200

    classe PhoneNumber, main

--- Overloading_operatori/class_PhoneNumber/main.cpp
+++ Overloading_operatori/class_PhoneNumber/main.cpp
@@ -1,45 +1,15 @@
 #include <iostream>
+#include <cstdlib>
 using namespace std;
 
 #include "PhoneNumber.h"
-#include "Array.h"
-#include "String.h"
 
 int main() {
-    cout << "--- CLASS PhoneNumber.h ---" << endl;
     PhoneNumber ogg;
     cout << "Enter phonenumber in form (111) 111-1111:" << endl;
     cin >> ogg;
     cout << "The phone number is: ";
     cout << ogg << endl;
 
-    cout << "\n--- CLASS Array.h ---" << endl;
-    Array obj1(6);
-    Array obj2(3);
-
-    if (obj1 != obj2)
-    	cout << "array diversi" << endl;
-    else
-    	cout << "array uguali" << endl;
-
-    cout << obj1.getSize() << " - " << obj2.getSize() << endl;
-    
-    obj2 = obj1;
-    
-    cout << obj1.getSize() << " - " << obj2.getSize() << endl;
-
-    if (obj1 == obj2)
-    	cout << "array uguali" << endl;
-    else
-    	cout << "array diversi" << endl;
-
-    cout << "Inserisci il valore: " ; 
-    cin >> obj1 >> obj2 ;
-
-    cout << obj1[1] << endl;
-    obj1[0] = 10;
-    cout << obj1[0] << endl;
-    cout << obj1[21] << endl;
-    
-    return(0);
+    return(EXIT_SUCCESS);
 }
