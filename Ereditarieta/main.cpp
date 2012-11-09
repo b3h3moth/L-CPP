e48185cf33d1e294ac0a02cbd5243f7a718f138b
B3h3m0th <behemoth@autistici.org>
2012-11-09 12:08:10 +0100

    test con dai membro protected

--- Ereditarieta/main.cpp
+++ Ereditarieta/main.cpp
@@ -7,13 +7,24 @@ using namespace std;
 #include "CommissionEmployeePlus.h"
 
 int main() {
-    CommissionEmployee impiegato("liuc", "stack","12337813", 100, .04);
-    impiegato.print();
-
-    cout << "\n\nguadagno impiegato: $" << impiegato.earnings();
-
+    // Istanzia un oggetto della classe derivata
     CommissionEmployeePlus impiegato2("frank", "ross", "6785678", 
             5000, .04, 300);
+
+    cout << fixed << setprecision(2);
+
+    cout << "Info impiegato ottenute mediante funzioni accessor 'get': \n"
+         << "       Nome: " << impiegato2.getFirstName()
+         << "\n    Cognome: " << impiegato2.getLastName()
+         << "\n SSN (INPS): " << impiegato2.getSSN()
+         << "\n  Fatturato: " << impiegato2.getGrossSales()
+         << "\nPercentuale: " << impiegato2.getCommissionRate()
+         << "\n      Fisso: " << impiegato2.getBaseSalary() << endl;
+
+    // Imposta lo stipendio fisso mensile
+    impiegato2.setBaseSalary(1000);
+
+    cout << "\nInfo impiegato ottenute mediante funzione print() : \n" << endl;
 
     impiegato2.print();
     cout << "\n\nguadagno impiegato: $" << impiegato2.earnings();
