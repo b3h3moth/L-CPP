fa8932aaed79b1eaca8d0e037dd5b5779b2b180c
B3h3m0th <behemoth@autistici.org>
2012-12-12 11:34:11 +0100

    main, argomenti

--- Ereditarieta/costruttori_distruttori/argomenti_al_costruttore/Cube.h
+++ Ereditarieta/costruttori_distruttori/argomenti_al_costruttore/Cube.h
@@ -10,6 +10,28 @@ class Cube : public Rectangle
         // Costruttore di default
         Cube() : Rectangle() { height = 0.0; volume = 0.0; }
 
+        /* Si noti l'utilizzo del costruttore della classe derivata seguito dal
+        costruttore della classe base. In fase di esecuzione, il costruttore 
+        della classe derivata Cube() invochera' anzitutto il costruttore della 
+        classe base Rectangle(), al quale, come nel secondo costruttore, possono
+        essere passati degli argomenti. 
+       
+        Da notare che in questo caso il costruttore della classe derivata e'
+        stato implementato inline, se si fosse optato per una definizione
+        esterna alla classe, il prototipo del costruttore sarebbe stato:
+        
+        Cube(double w, double, l, double h);
+        
+        La definizione invece sarebbe stata:
+        
+        Cube::Cube(double w, double l, double h) : Rectangle(w, l) { 
+            height = h;
+            volume = getArea() * h; }
+
+        Il costruttore della classe base e' sempre eseguito prima del
+        costruttore della classe derivata. 
+        */
+
         // Costruttore con argomenti
         Cube(double w, double l, double h) : Rectangle(w, l) { 
             height = h;
--- Ereditarieta/costruttori_distruttori/argomenti_al_costruttore/main.cpp
+++ Ereditarieta/costruttori_distruttori/argomenti_al_costruttore/main.cpp
@@ -5,5 +5,27 @@ using namespace std;
 #include "Cube.h"
 
 int main() {
+    double cube_width;
+    double cube_height;
+    double cube_lenth;
+
+    cout << "Dimensioni del cubo" << endl;
+    cout << "Larghezza: " ;
+    cin >>  cube_width;
+    cout << "Altezza: " ;
+    cin >>  cube_height;
+    cout << "Lunghezza: " ;
+    cin >>  cube_lenth;
+
+    // Si istanzia un oggetto Cube
+    Cube obj(cube_width, cube_height, cube_lenth);
+
+    cout << "\nProprieta' di un oggetto Cube" << endl;
+    cout << "Larghezza: " << obj.getWidth() << endl;
+    cout << "Altezza: " << obj.getHeight() << endl;
+    cout << "Lunghezza: " << obj.getLength() << endl;
+    cout << "Area: " << obj.getArea() << endl;
+    cout << "Volume: " << obj.getVolume() << endl;
+
     return(EXIT_SUCCESS);
 }
