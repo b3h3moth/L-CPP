3fed1c640ca04bd8a95f27c04bd1c19ecd280f0f
B3h3m0th <behemoth@autistici.org>
2012-12-13 09:56:13 +0100

    classe Car

--- Ereditarieta/costruttori_distruttori/argomenti_al_costruttore/class_Automobile/Car.h
+++ Ereditarieta/costruttori_distruttori/argomenti_al_costruttore/class_Automobile/Car.h
@@ -1,12 +1,29 @@
 #ifndef CAR_H
 #define CAR_H
 
+#include <string>
 #include "Automobile.h"
+using namespace std;
 
+/* La classe Car e' una specializzazione della classe Automobile, fornisce come
+informazione aggiuntiva il numero delle portiere di una macchina */
 class Car : public Automobile
 {
     public:
+        // Costruttore di default
+        Car() { portiere = 0; }
+
+        // Costruttore con argomenti, passati al costruttore della classe base
+        Car(string marca_car, int modello_car, int kilometraggio_car, 
+                double prezzo_car, int portiere_car) :
+            Automobile(marca_car, modello_car, kilometraggio_car, prezzo_car) {
+                portiere = portiere_car;
+            }
+
+        int getPortiere() const { return portiere; }
+
     private:
+        int portiere;
 };
 
 #endif
