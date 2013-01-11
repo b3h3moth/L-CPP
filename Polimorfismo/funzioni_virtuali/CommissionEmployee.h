5e33bb96bfabffa67209558ed507dfe5d3763959
B3h3m0th <behemoth@autistici.org>
2013-01-11 12:39:56 +0100

    funzioni virtuali, aggiunta info

--- Polimorfismo/funzioni_virtuali/CommissionEmployee.h
+++ Polimorfismo/funzioni_virtuali/CommissionEmployee.h
@@ -29,12 +29,29 @@ class CommissionEmployee
         /* Calcola la retribuzione dell'agente di vendita
 
         Nelle funzioni virtuali il tipo dell'oggetto puntato, e non il tipo di
-        handle, determina quale funzione utilizzare.
+        handle, determina quale funzione utilizzare, detto in parole povere, una
+        funzione virtuale viene dinamicamente associata alla chiamata di una 
+        funzione, dinamic binding.
+
+        Dinamic Binding
+        ---------------
+        Il C++ col dinamic binding riesce a determinare a runtime, verificando 
+        il tipo dell'oggetto responsabile della chiamata, quale funzione
+        utilizzare. 
 
         Il polimorfismo si realizza invocando una funzione virtuale attraverso
         un puntatore o un riferimento alla classe base; il C++ determinera'
         dinamicamente la versione corretta della funzione da adoperare nella 
-        classe derivata */
+        classe derivata.
+       
+        In definitiva, cio' che dev'essere chiaro e' che e' il tipo dell'oggetto
+        chiamante l'attore principale e cio' a cui si deve fare attenzione.
+        
+        Una funzione virtuale si dichiara aggiungendo la parola chiave 'virtual'
+        prima del tipo di ritorno nelle dichiarazioni delle funzioni della
+        classe base.  */
+        
+        // Calcola la retribuzione dell'agente di vendita
         virtual double earnings() const;
         virtual void print() const;
 
