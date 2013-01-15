debd87e971757dd5c06aa8c813ec3e0e8abecfc5
B3h3m0th <behemoth@autistici.org>
2013-01-15 11:07:07 +0100

    template di funzione, stampa array

---
+++ Template/template_di_funzione/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+// Definizione del template di funzione
+template<typename T>
+void stampaArray(const T *array, int count)
+{
+    for (int i = 0; i<count; i++)
+        cout << array[i] << " ";
+
+    cout << endl;
+}
+
+int main() {
+    const int dim1 = 10;   // Dimensione array 1
+    const int dim2 = 12;   // Dimensione array 2
+    const int dim3 = 13;   // Dimensione array 3
+
+    int arr1[dim1] = {1,2,3,4,5,6,7,8,9,10};
+    float arr2[dim2] = {1.1,2.2,3.3,4.4,5.5,6.6,7.7,8.8,9.9,10.10,11.11,12.12};
+    char arr3[dim3] = "C++ Template";
+
+    // Invoca la specializzazione 'int' del template
+    stampaArray(arr1, dim1);
+    
+    // Invoca la specializzazione 'float' del template
+    stampaArray(arr2, dim2);
+    
+    // Invoca la specializzazione 'char' del template
+    stampaArray(arr3, dim3);
+
+    return(EXIT_SUCCESS);
+}
