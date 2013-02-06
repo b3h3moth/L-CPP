57abb18da361e31a1f99a9d283534bd07af1ee1e
B3h3m0th <behemoth@autistici.org>
2013-02-06 18:48:21 +0100

    RTTI introduzione

---
+++ Polimorfismo/RTTI_Run_Time_Type_Information/dynamic_cast.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+/* RTTI (Run-Time Type Identification, Identificazione dei tipi a tempo di 
+esecuzione), consente a programmi che manipolano oggetti come puntatori o 
+riferimenti a classi base, di determinare l'effettivo tipo derivato a cui essi
+si riferiscono.
+
+*/
+
+int main() {
+    return(EXIT_SUCCESS);
+}
