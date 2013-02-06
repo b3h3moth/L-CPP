0ef3db51bca2c1123a0fed466c06d3430fa4f196
B3h3m0th <behemoth@autistici.org>
2013-02-06 19:15:18 +0100

    dynamic_cast (a.k.a. safe downcasting) intro

--- Polimorfismo/RTTI_Run_Time_Type_Information/dynamic_cast.cpp
+++ Polimorfismo/RTTI_Run_Time_Type_Information/dynamic_cast.cpp
@@ -7,6 +7,15 @@ esecuzione), consente a programmi che manipolano oggetti come puntatori o
 riferimenti a classi base, di determinare l'effettivo tipo derivato a cui essi
 si riferiscono.
 
+dynamic_cast, consente la conversione di un puntatore ad una classe base in un
+puntatore ad una classe derivata, oppure un lvalue che si riferisce ad una 
+classe base in un riferimento a una classe derivata; affinche' le informazioni
+sul tipo della classe derivata possano essere rilevate, e' necessario che
+l'operando di dynamic_cast dev'essere una classe con una o piu' funzioni
+virtuali.
+
+Nota: La conversione di un puntatore a una classe base in un puntatore a una
+      classe derivata e' detto "safe downcasting".
 */
 
 int main() {
