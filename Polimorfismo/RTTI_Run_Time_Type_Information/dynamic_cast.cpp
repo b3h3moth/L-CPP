d48da6305d189cd0ad5a98f2663973592fb942d0
B3h3m0th <behemoth@autistici.org>
2013-02-07 11:18:16 +0100

    RTTI; dynamic_cast, esempio completo

--- Polimorfismo/RTTI_Run_Time_Type_Information/dynamic_cast.cpp
+++ Polimorfismo/RTTI_Run_Time_Type_Information/dynamic_cast.cpp
@@ -15,9 +15,60 @@ l'operando di dynamic_cast dev'essere una classe con una o piu' funzioni
 virtuali.
 
 Nota: La conversione di un puntatore a una classe base in un puntatore a una
-      classe derivata e' detto "safe downcasting".
+      classe derivata e' detto "downcasting" oppure anche "upcasting", poiche' 
+      si intende eseguire il casting da un antenato a un discendente.
 */
 
+class Animale
+{
+    public:
+        virtual void verso() = 0;
+        
+        // Distruttore
+        virtual ~Animale() {};
+};
+
+class Cane : public Animale
+{
+    public:
+        void verso() { cout << "Il cane abbaia" << endl; };
+        void caratteristica() { cout << "Il cane e' fedele" << endl; };
+};
+
+class Gatto : public Animale
+{
+    public:
+        void verso() { cout << "Il gatto miagola" << endl; };
+        void costo() { cout << "Il persiano e' un gatto costoso" << endl; }
+};
+
+void TipoDiAnimale(Animale *panimale)
+{
+    Cane *pcane     = dynamic_cast<Cane*>(panimale);
+    Gatto *pgatto    = dynamic_cast<Gatto*>(panimale);
+
+    if (pcane) {
+        cout << "Sono un cane" << endl;
+        pcane->caratteristica();
+    }
+
+    if (pgatto) {
+        cout << "Sono un gatto" << endl;
+        pgatto->costo();
+    }
+
+    cout << "Verifica del tipo usando virtual Animale::verso " << endl;
+    panimale->verso();
+}
+
+
 int main() {
+    Cane cane;
+    Gatto gatto;
+
+    TipoDiAnimale(&cane);
+    cout << endl;
+    TipoDiAnimale(&gatto);
+
     return(EXIT_SUCCESS);
 }
