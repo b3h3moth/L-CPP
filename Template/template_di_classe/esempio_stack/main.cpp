7439811f669d20134c352a0135ca590b35ad5d27
B3h3m0th <behemoth@autistici.org>
2013-03-15 11:26:41 +0100

    template: stack, esempio con double

--- Template/template_di_classe/esempio_stack/main.cpp
+++ Template/template_di_classe/esempio_stack/main.cpp
@@ -23,7 +23,25 @@ int main() {
     while (int_stack.pop(elem_int))
         cout << elem_int << ' ';
 
-    cout << "stack vuoto " << endl;
+    cout << "stack vuoto\n" << endl;
 
+    Stack<double> dbl_stack(10);
+    double elem_dbl = 1.1;
+
+    cout << "Inserimento elemento di tipo double nello stack" << endl;
+
+    // Inserisce gli elementi nello stack
+    while (dbl_stack.push(elem_dbl)) {
+        cout << elem_dbl << ' ';
+        elem_dbl++;
+    }
+
+    cout << "Stack pieno, push(" << elem_dbl << ") non eseguibile" << endl;
+
+    // Estrae gli elementi dallo stack
+    while (dbl_stack.pop(elem_dbl))
+        cout << elem_dbl << ' ';
+
+    cout << "stack vuoto " << endl;
     return(EXIT_SUCCESS);
 }
