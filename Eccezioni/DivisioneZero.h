5955ee08b17b6a7f6633b4effa67359dc74ca360
B3h3m0th <behemoth@autistici.org>
2013-03-23 19:42:20 +0100

    Eccezioni: divisione per zero, intro all'argomento

---
+++ Eccezioni/DivisioneZero.h
@@ -0,0 +1,28 @@
+#ifndef DIVISIONEZERO_H
+#define DIVISIONEZERO_H
+
+#include <stdexcept>
+using std::runtime_error;
+
+/* Lo scopo e' di prevenire la divisione per zero, che in C++, qualora dovesse
+accadere, provocherebbe la terminazione del programma; la classe DivisioneZero
+si occupa della gestione dell'eccezione.
+
+La classe runtime_error della libreria standard e' una specializzazione della 
+classe exception, si occupa della gestione degli errori a runtime (tempo di 
+esecuzione), mentre la classe DivisioneZero e' a sua volta una specializzazione
+della classe runtime_error.
+
+La classe exception e' la classe base, o superclasse, delle eccezioni in C++,
+l'header di riferimento e' <stdexcept>.
+*/
+
+
+class DivisioneZero : public runtime_error
+{
+    public:
+        // Il costruttore si occupa della definizione del messaggio d'errore
+        DivisioneZero() : runtime_error("Attenzione, si sta dividendo per 0") {}
+};
+
+#endif
---
+++ Eccezioni/main.cpp
@@ -0,0 +1,43 @@
+#include <cstdlib>
+#include <iostream>
+using namespace std;
+
+#include "DivisioneZero.h"
+
+double quoziente(int numeratore, int denominatore);
+
+int main() {
+    int num1, num2;
+    double risultato;
+
+    cout << "Inserire due numeri interi (^C quit): ";
+
+    while (cin >> num1 >> num2) {
+        try {
+            risultato = quoziente(num1, num2);
+            cout << "Risultato: " << risultato << endl;
+        }
+
+        catch (DivisioneZero &DivisioneZero) {
+            cout << "Eccezione: " << DivisioneZero.what() << endl;
+        }
+        
+        cout << "Inserire due numeri interi (^C quit): ";
+    }
+
+    cout << endl;
+
+    return(EXIT_SUCCESS);
+}
+
+/* La funzione quoziente, si occupera' sia della eventuale divisione sia del
+lancio dell'eccezione. */
+double quoziente(int numeratore, int denominatore)
+{
+    // Lancia una eccezione DivisioneZero se si tenta di dividere per 0
+    if (denominatore == 0)
+        throw DivisioneZero();
+
+    // Restituisce il risultato della divisione, se tutto procede correttamente
+    return static_cast<double>(numeratore) / denominatore;
+}
