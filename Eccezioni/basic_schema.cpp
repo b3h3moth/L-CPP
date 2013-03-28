e5eef99d3673eeea615303bdab8de66e1dfdff92
B3h3m0th <behemoth@autistici.org>
2013-03-28 10:14:23 +0100

    eccezioni: scheletro di base

---
+++ Eccezioni/basic_schema.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+double divisione_zero(int num, int den);
+
+int main() {
+    int n_num, n_den;
+    double divisizione;
+
+    cout << "Inserire due numeri: " ;
+    cin >> n_num >> n_den;
+
+    try {
+        divisizione = divisione_zero(n_num, n_den);
+        cout << "divisione: " << divisizione << endl;
+    } catch (string excstring) {
+        cout << excstring << endl;
+    }
+
+    cout << "fine" << endl;
+
+    return(EXIT_SUCCESS);
+}
+
+double divisione_zero(int num, int den)
+{
+    /* Allorquando occorre un'eccezione, essa viene lanciata (throw); il
+    parametro di throw viene utilizzato per individuare il tipo di eccezione.
+    */
+    if (den == 0) {
+        string excstr = "Divisione per zero";
+        throw excstr; // Punto di lancio (throw point)
+    }
+
+    return static_cast<double>(num/den);
+}
