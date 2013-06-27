5daad1256d83a2f74c05845e764e1e1e46e953e0
B3h3m0th <behemoth@autistici.org>
2013-06-27 18:26:36 +0200

    ifstream, ofstream, fstream

--- File/01_file_sequenziale.cpp
+++ File/01_file_sequenziale.cpp
@@ -4,24 +4,31 @@
 using namespace std;
 
 /* L'header <fstream> contiene tutte le dichirazioni necessarie per lavorare con
-i file, definisce peraltro i tipi di dato ifstream, ofstream, e fstream, per 
-poter lavorare coi file infatti deve essere prima di tutto definito un oggetto 
+i file, definisce peraltro i tipi di dato: 
+- ifstream, input file stream, apre un file esistente e legge il contenuto;
+- ofstream, output file stream, crea un file in cui scrivere; 
+- fstream, file stream; apre un file per lettura, scrittura o entrambi.
+
+Nota: per poter lavorare coi file deve essere prima di tutto definito un oggetto
 di uno dei tre tipi.
+*/
 
 
 int main() {
-    // Si apre il file, se non esiste lo crea
+    int age;
+    char name[10];
+    char surname[10];
+
+    // Si crea un file all'interno del quale scrivere dei dati.
     ofstream out_file("db.txt", ios::out);
 
+    // Si verifica l'assenza di errori
     if (!out_file) {
         cerr << "File non trovato" << endl;
         exit(EXIT_FAILURE);
     }
 
     cout << "nome, cognome, eta': " << endl;
-    int age;
-    char name[10];
-    char surname[10];
 
     while (cin >> name >> surname >> age) {
         out_file << name << ' ' << surname << ' ' << age << endl;
